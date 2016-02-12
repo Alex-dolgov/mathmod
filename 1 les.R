c901cb92f190a5c9080c36c0d64cfc3d2110c4b4
yaroslavtsevam <yaroslavtsevam@gmail.com>
2016-02-12 16:20:59 +0300

    2less

--- 1 les.R
+++ 1 les.R
@@ -1,5 +1,38 @@
-j=rnorm(-10:10)
-j
-order(j)
-sort(j)
-show(j[order(j)])
+f=rep(c('mouse','rat','fly'),c(5,7,11))
+x=factor(f)
+summary(f)
+summary(x)
+
+
+y=1:100
+summary(y)
+summary(y)[1]
+summary(y)["Min."]
+summary(x)["rat"]
+
+
+d=rep(c("A","T","G","C"), c(2880,1478,3890,7600))
+s=factor(d)
+summary(d)
+summary(s)
+length(s)
+q=summary(s)/length(s)
+
+
+sample(d, size = 84000, replace = TRUE)
+q2=sample(d, size = 84000, replace = TRUE)
+x2=factor(q2)
+summary(q2)
+summary(x2)
+length(x2)
+a=summary(x2)/length(x2)
+q-a
+
+
+ iris[3,]
+ iris[,3]
+iris[iris$Species==c('setosa','versicolor'),c('Sepal.Length','Petal.Length')]
+z=factor(iris$Petal.Length)
+m=iris[iris$Petal.Length>2 & iris$Petal.Length<6,c('Species')]
+summary(factor(m))
+
