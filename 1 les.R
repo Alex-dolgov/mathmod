389ea7be3267fb5823aed6a8fcc45a8d16117222
yaroslavtsevam <yaroslavtsevam@gmail.com>
2016-03-04 14:42:12 +0300

    yo i am raper

--- 1 les.R
+++ 1 les.R
@@ -36,3 +36,23 @@ z=factor(iris$Petal.Length)
 m=iris[iris$Petal.Length>2 & iris$Petal.Length<6,c('Species')]
 summary(factor(m))
 
+======Занятие №4==========
+  
+eddy=read.csv('eddy.csv',skip = 1)
+eddy=eddy[-1,]
+write.csv(eddy,'eddy2.csv')
+eddy2=read.csv('eddy2.csv')
+summary(eddy2)  
+
+stat.desc(eddy2)
+
+ggplot(eddy2,aes(h2o))+geom_histogram()
+eddy2==-9999
+eddy2[eddy2==-9999]= NA
+
+
+x=na.exclude(eddy2$h2o_strg)
+fitdistr(x,'normal')
+
+ks.test(x,pnorm,mean=y[1],sd=y[2])
+y=fitdistr(x,'normal')$estemate
